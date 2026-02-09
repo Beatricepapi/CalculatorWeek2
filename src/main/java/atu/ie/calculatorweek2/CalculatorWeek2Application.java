6531de7e7ac852f7b1cdc52f2ec9b6a41b503b30
Beatrice Papi <G00461762@atu.ie>
2026-02-09 11:39:22 +0000

    Project set up

---
+++ src/main/java/atu/ie/calculatorweek2/CalculatorWeek2Application.java
@@ -0,0 +1,13 @@
+package atu.ie.calculatorweek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWeek2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorWeek2Application.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/calculatorweek2/CalculatorWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.calculatorweek2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWeek2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
